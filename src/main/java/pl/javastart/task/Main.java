9b5246e030fdddddfe6bd89331f0fc97d1d735a3
Julka Czarnasiak <czarna13500@gmail.com>
2023-07-09 14:36:44 +0200

    Add second part of the task

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,15 +3,9 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-        Phone phone1 = new Phone();
-        phone1.brand = "Samsung";
-        phone1.model = "Galaxy S6";
-        phone1.color = "Black";
+        Phone phone1 = new Phone("Samsung", "Galaxy S6", "Black");
 
-        Phone phone2 = new Phone();
-        phone2.brand = "iPhone";
-        phone2.model = "X";
-        phone2.color = "Pink";
+        Phone phone2 = new Phone("iPhone", "X", "Pink");
 
         System.out.printf("Phone1: %s %s %s\n", phone1.brand, phone1.model, phone1.color);
         System.out.printf("Phone2: %s %s %s\n", phone2.brand, phone2.model, phone2.color);
--- src/main/java/pl/javastart/task/Phone.java
+++ src/main/java/pl/javastart/task/Phone.java
@@ -4,4 +4,10 @@ public class Phone {
     String brand;
     String model;
     String color;
+
+    Phone(String brand, String model, String color) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+    }
 }
